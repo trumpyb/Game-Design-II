b6b8ef2e6d008c80d8a42f63e80d2cdb1f49e2c9
trumpyb <91992867+trumpyb@users.noreply.github.com>
2024-05-07 15:13:57 -0500

    Update

---
+++ boid.gd
@@ -0,0 +1,6 @@
+extends Node2D
+
+#Avoid collision with other Boids
+#Try to go in the same direction with other Boids
+#Turn to center of mass with other Boids
+
--- rc_player.gd
+++ rc_player.gd
@@ -2,9 +2,9 @@ extends VehicleBody3D
 
 @export var MAX_STEER = .4
 @export var level = ""
-const MAX_RPM = 600
-const MAX_TORQUE = 400
-const HORSE_POWER = 400
+var MAX_RPM = 600
+var MAX_TORQUE = 400
+var HORSE_POWER = 400
 
 
 func _ready():
@@ -16,10 +16,21 @@ func calc_engine_force(accel, rpm):
 func _physics_process(delta):
 	check_and_right_vehicle()
 	
+	var accel = Input.get_axis("ui_down", "ui_up") * HORSE_POWER
+	
+	if Input.is_action_pressed("ui_accept"):
+		MAX_RPM = 1200
+		MAX_TORQUE = 800
+		HORSE_POWER = 800
+	else:
+		MAX_RPM = 600
+		MAX_TORQUE = 400
+		HORSE_POWER = 400
+	
 	steering = lerp(steering, Input.get_axis("ui_right", "ui_left") * MAX_STEER, delta * 5)
 	
 	
-	var accel = Input.get_axis("ui_down", "ui_up") * HORSE_POWER
+	
 	
 	$backLeft.engine_force = calc_engine_force(accel, abs($backLeft.get_rpm()))
 	$backRight.engine_force = calc_engine_force(accel, abs($backRight.get_rpm()))
